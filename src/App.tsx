ceff2260386dc8a03c4567f1009c1cb4f10ca2b6
Kauê <kaue0607@gmail.com>
2021-10-18 23:57:30 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback, useRef, useContext } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+interface User {
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+const App: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [users, setUsers] = useState<[User]>();
+
+  const names = useMemo(() => users?.map(user => user.name).join(', ') || '', [users]);
+
+  const greeting = useCallback(
+    (user: User) => alert(`Hello ${user.name}`),
+    []
   );
+
+  inputRef.current?.focus();
+
+  return (
+    <form action="">
+      <input type="text" ref={inputRef} />
+    </form>
+  )
 }
 
 export default App;
